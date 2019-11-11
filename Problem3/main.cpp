2520eac4d9401152de8e792e4b0080340c436dd4
MatthewGoin <42951553+MatthewGoin@users.noreply.github.com>
2019-11-11 12:45:44 -0700

    Update main.cpp

--- Problem3/main.cpp
+++ Problem3/main.cpp
@@ -10,28 +10,21 @@ int main()
     bool Prime = false;
     int counter = 0;
     int largest = 0;
-    //unsigned long long int num = 13195;
     unsigned long long int num = 600851475143;
     for(unsigned long long int i = 2; i < floor(num/2); i++ ){
         if(num % i == 0){
-            //cout << "Are we here" << endl;
             for(unsigned long long int j = 2; j < i; j++){
                 if(i % j == 0){
-                  
                     Prime = true;
                     break;
                 }
             }
-
             if(Prime == false){
                 counter++;
             }
-            //SKIP:
             else {
                 Prime = false;
-
             }
-
         }
         if(counter > 0){
             largest = i;
